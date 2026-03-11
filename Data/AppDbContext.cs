03f395a51bc545c434e9632bb11155957aec4c95
Hashim Aziz Muhammad <hashim.muhammed@boxfusion.io>
2026-03-11 09:16:04 +0200

    Add Entity Framework Core with SQL Server support
    Added EF Core packages and configured SQL Server connection. Introduced AppDbContext with DbSet<Character>. Registered DbContext and character service in DI. Updated appsettings.json with connection string for local SQL Server.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using DotNetEFTestBox.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetEFTestBox.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(options)
+    {
+
+        public DbSet<Character> Characters => Set<Character>();
+    }
+}
